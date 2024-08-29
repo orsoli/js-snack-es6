a9372e0f8a754c57a2c21e4befdc45b838f0b296
orsoli <fljcoli@gmail.com>
2024-08-29 14:54:26 +0200

    first commit

---
+++ js/structure.js
@@ -0,0 +1,12 @@
+/* SNACK 1
+Dwayne Johnson has asked us to create place cards for the guest table at his mega VIP party. 
+He left us the name of the table ("VIP Table") and the list of guests in seating order:
+['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+But the typography company that will print everything wants us to send them a list of guests in a specific format, 
+so we need to ensure that each guest is a JavaScript object with the following properties:
+    -table name (tableName),
+    -guest name (guestName),
+    -place occupied (place).
+
+Generate and print the list for the place cards in the console.
+*/
